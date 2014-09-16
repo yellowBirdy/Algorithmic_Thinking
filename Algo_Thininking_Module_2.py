fe1df9849f56d2188fd7f8426e316436314eb02b
yellowBirdy <mieszkomieszko@gmail.com>
2014-09-17 00:03:36 +0200

    My Algorithmic Thinking course repo, first commit

---
+++ Algo_Thininking_Module_2.py
@@ -0,0 +1,141 @@
+
+# coding: utf-8
+
+## 1. Project
+
+# In[1]:
+
+"""
+Project 2 of Algorythmic Thinking implementation: 
+1. implementing BFS algorithm
+2. computing CC of undirected graph using BFS algorithm
+3. computing resilence of a network
+"""
+
+#import O(1) queue implementation
+from collections import deque
+import random
+
+
+# In[37]:
+
+#small fully connected graph
+EX_UGRAPH0 = {0:set([1,2,3]),
+              1:set([0,2]),
+              2:set([0,1]),
+              3:set([0])}
+
+#small graph with 2 CC
+EX_UGRAPH1 = {0:set([1,2,3]),
+              1:set([0,2]),
+              2:set([0,1]),
+              3:set([0]),
+              4:set([5]),
+              5:set([4])}
+
+#slightly bigge graph with 4 CC and largest CC size 5
+EX_UGRAPH2 = {0:set([1,3,5]),
+              1:set([0]),
+              2:set([]),
+              3:set([0,5]),
+              4:set([7,8]),
+              5:set([0,3,9]),
+              6:set([]),
+              7:set([4,8]),
+              8:set([4,7]),
+              9:set([5])
+              }
+
+
+### Task 1: BFS-Visited implementation
+
+# In[3]:
+
+
+
+def bfs_visited(ugraph, start_node):
+    """
+    Implementation of BFS-visited alogorithm 
+    takes 2 args, undirected graph as adjacency list and the start node
+    returns a set of all visited nodes - a connected component of the start node
+    """
+    queue = deque()
+    visited = set()
+    visited = visited | set([start_node])
+    queue.append(start_node)
+    while queue:
+        #deque
+        current_node = queue.popleft()
+        for neighbour in ugraph[current_node]:
+            if neighbour not in visited:
+                visited = visited | set([neighbour])
+                queue.append(neighbour)
+    #end of queue
+    return visited
+
+
+
+### Task2: Computing a set of connected components CC using BFS-Visited
+
+# In[4]:
+
+def cc_visited(ugraph) :
+    """ 
+    computes all connected components using BFS-visited algorithm
+    takes 1 arg: undirected graph as adjacency list
+    Returns a list of all connected components of ugraph
+    """
+    remaining_nodes = set(ugraph.keys())
+    connected_components = []
+    while remaining_nodes:
+        start_node = random.sample(remaining_nodes,1)[0]
+        #computes connected component of start_node and adds it to the list of all connected components
+        cc_of_start_node = bfs_visited(ugraph,start_node)
+        connected_components.append(cc_of_start_node)
+        #removes all nodes from computed connected component from remaining nodes
+        remaining_nodes = remaining_nodes - cc_of_start_node
+        
+    return connected_components
+
+
+def largest_cc_size(ugraph):
+    """ 
+    Computes the size of largest connected component of a ugraph
+    takes 1 arg: undirected graph as adjacency list
+    returns integer size of largest connected component
+    """
+    connected_components = cc_visited(ugraph)
+    largest_cc = 0
+    for component in connected_components:
+        cc_size=len(component)
+        if cc_size > largest_cc:
+            largest_cc = cc_size
+    
+    return largest_cc
+        
+
+
+### Part3: Resilence test
+
+# In[19]:
+
+def compute_resilience(ugraph, attack_order):
+    """
+    Computes the size of largest connected component of a ugraph as a funcion of attacked nodes
+    takes 2 args: undirected graph as adjacency list and a list of attacked nodes 
+    returns a list of sizes of largest connected component, first elelment is for full network followed by size after noeds have been attacked
+    """
+    #copy graph  to local variable 
+    local_ugraph = ugraph.copy()
+    largest_cc = []
+    largest_cc.append(largest_cc_size(local_ugraph))
+    for attacked_node in attack_order:
+        attacked_neighbors = local_ugraph[attacked_node]
+        for neighbor in attacked_neighbors:
+            local_ugraph[neighbor].remove(attacked_node)
+        del local_ugraph[attacked_node]
+        largest_cc.append(largest_cc_size(local_ugraph))
+    
+    return largest_cc
+    
+
---
+++ Algo_Thinking_module_1-in_degree_calc.py
@@ -0,0 +1,125 @@
+#! /usr/bin/python
+
+"""
+This is mini project for Module 1 of Algorithmic Thinking 
+course on Coursera from Rice Uni. 
+The script calculates in-degree as well as in- degree dist
+for digraphs represented by adjacency lists. It uses 3 test
+digraphs and also introduces a function to generate a full graph
+
+Added a function normalizing in_degree, and fixed in_degree calculating algorithm
+Added a function prining the normalized in_degree distibution in log scale
+"""
+#imports
+import matplotlib.pyplot as plt
+
+
+EX_GRAPH0 = {0:set([1,2]),
+             1:set(),
+             2:set()}
+
+EX_GRAPH1 = {0:set([1,4,5]),
+             1:set([2,6]),
+             2:set([3]),
+             3:set([0]),
+             4:set([1]),
+             5:set([2]),
+             6:set()}
+
+EX_GRAPH2 = {0:set([1,4,5]),
+             1:set([2,6]),
+             2:set([3,7]),
+             3:set([7]),
+             4:set([1]),
+             5:set([2]),
+             6:set([]),
+             7:set([3]),
+             8:set([1,2]),
+             9:set([0,3,4,5,6,7])}
+
+
+
+def make_complete_graph(num_nodes) :
+    """ 
+    Generates a full digraph ("undigraph" as well in fact) 
+    with num_nodes nodes for positive num_nodes otherwise generates 
+    an empty graph. Uses adjacency list format ie. dictionary. 
+    """
+
+    the_graph = {}
+    if num_nodes > 0 :  
+        for vert in range(num_nodes):
+            edges = set(range(vert)).union(set(range(vert+1,num_nodes)))
+            the_graph[vert] = edges
+    return the_graph
+
+def compute_in_degrees(digraph):
+    """ computes in degrees of each node of diagraph
+    returns a dictionary with indegrees of each node """
+    #initialize the in_degree
+    in_degrees = {}
+    for vert in digraph.keys():
+        in_degrees[vert] = 0 
+        
+    for vert in digraph.keys():
+        for in_vert in digraph[vert]:
+            in_degrees[in_vert] += 1
+               
+    return in_degrees
+
+
+def in_degree_distribution(digraph):
+    """ computes distribution of in degrees of a digraph with aid 
+    of compute_in_degree function """
+    in_degr = compute_in_degrees(digraph)
+    
+    in_degree_dist = {}
+    #uses set on the degrees to remove duplicates, sorting not needed as they 
+    #will server as keys for a dictionary
+    for degr in set(val for val in in_degr.values()):
+        in_degree_dist[degr] = in_degr.values().count(degr) 
+    
+    return in_degree_dist
+
+
+###code for normalization of degree distribution
+
+def in_degree_distribution_normalized(digraph):
+    """
+    Calculates a normalized (ie. to relative frequecies) in_degree distribution of a digraph.
+    by normalizing result of a call unnormalized degree distribtuion function.
+    Takes a digraph defined as an adjacency list. 
+    Returns a dictionary with relative frequecies of all degrees found in the input digraph
+    """
+    degree_distribution = in_degree_distribution(digraph)
+    #calculate total count
+    total_edges = 0.0
+    for degree in degree_distribution.keys():
+        total_edges += degree_distribution[degree]
+    #normalize
+    normalized_distribution = {}
+    for degree in degree_distribution.keys():
+        normalized_distribution[degree] = degree_distribution[degree]/total_edges
+    
+    return normalized_distribution
+
+###code for printing the graph
+
+def plot_indegree_distribution(in_degree_distribution):
+    """
+    plots a normalized empirical in_degree distribution in log scale(would plot any degree disto if lables and title changed)
+    """
+    plt.figure()
+    plt.plot(in_degree_distribution.values(),in_degree_distribution.keys(),'ro')
+    plt.xlabel ('Indegree of nodes (log)')
+    plt.ylabel ('Relative frequency of Indegree (log)')
+    plt.title ('Normlized Indegree Distibution in log/log scale')
+    plt.grid(True)
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.show()
+
+#####test
+#print in_degree_distribution(EX_GRAPH1)
+
+#plot_indegree_distribution (in_degree_distribution_normalized(EX_GRAPH2))
